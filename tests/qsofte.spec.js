d05bf4a0856cb3a22c790cbb5b388653efea731f
ashutoshqsofte <ashutosh@qsofte.com>
2024-08-29 12:35:18 +0530

    changed url

--- tests/qsofte.spec.js
+++ tests/qsofte.spec.js
@@ -1,11 +1,11 @@
 const { test, expect } = require("@playwright/test");
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3000/Connect");
+  await page.goto("http://test.qsofte.com/Connect");
 
   await expect(page).toHaveTitle("Qsofte");
 });
 
 test("get started link", async ({ page }) => {
-  await page.goto("http://localhost:3000/Connect");
+  await page.goto("http://test.qsofte.com/Connect");
 });
